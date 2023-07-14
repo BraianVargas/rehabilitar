e7d3a97162a979bbada6dd1a451514f2c24c43d7
Facundo Paez <f.fpaez97@gmail.com>
2023-07-14 12:57:24 -0300

    configuring http request

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpRequest } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import { LoginUser } from '../models/login-user';
 import { Token } from '../models/token';
@@ -14,6 +14,19 @@ export class LoginService {
   constructor(private httpClient: HttpClient) { }
 
   public login(loginUser: LoginUser): Observable<Token> {
-    return this.httpClient.post<Token>('/login', loginUser) // this.loginURL + 
+
+    const data = JSON.stringify({
+      'data': loginUser
+    })
+
+    console.log( loginUser )
+    console.log(data)
+
+    return this.httpClient.post<Token>('/login', data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
+  
 }
