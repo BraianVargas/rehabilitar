84c3ea93fde02c2326bdafc5b471cedba10d3527
Facundo Paez <f.fpaez97@gmail.com>
2023-07-31 20:26:23 -0300

    [UPDATE] upload-file component and report service

--- src/app/components/upload-file/upload-file.component.ts
+++ src/app/components/upload-file/upload-file.component.ts
@@ -1,6 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CompanyService } from 'src/app/services/company.service';
+import { PatientService } from 'src/app/services/patient.service';
 import { ReportService } from 'src/app/services/report.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -9,17 +13,133 @@ import { ReportService } from 'src/app/services/report.service';
 })
 export class UploadFileComponent {
   // info: boolean = false;
+  // public archivos: any = [];
+  // public archivo: any;
+
+  public files!: FileList | null;
+
+  myForm!: FormGroup;
 
   constructor(
     private companyService: CompanyService,
-    private reportService: ReportService
+    private reportService: ReportService,
+    private fb: FormBuilder,
+    private patientService: PatientService,
+    private tokenService: TokenService
   ) {}
 
   ngOnInit() {
     // this.info = this.companyService.thereIsCompany;
+
+    this.myForm = this.fb.nonNullable.group({
+      file: this.fb.nonNullable.group
+    });
   }
 
-  subirArchivo() {
-    this.reportService.fileUpload();
+  // subirArchivo() {
+  //   this.reportService.fileUpload();
+  // }
+
+  captureFile( event: any ) {
+    // const data = JSON.stringify({
+    //   'token': this.tokenService.getToken(),
+    //   'data': {
+    //     'paciente': this.patientService.getIdPaciente(),
+    //     'DNI': this.patientService.getDniPaciente(),
+    //     'empresa': this.companyService.getIdEmpresa()
+    //   }
+    // });
+
+    // const document = event.target.files[0];
+    const document2 = event.target as HTMLInputElement;
+
+    this.files = document2.files;
+
+    // if( files!.length > 0 && files != null ) {
+    //   const formData2 = new FormData();
+
+    //   Array.prototype.forEach.call( files, (file: File) => {
+    //     formData2.append('document', file);
+    //   } );
+
+    //   formData2.append('json', data);
+
+    //   this.reportService.fileUpload(formData2).subscribe({
+    //     next: (data) => {
+    //       console.log(data);
+          
+    //     },
+    //     error: (err: HttpErrorResponse) => {
+    //       console.log(err);
+          
+    //     },
+    //     complete: () => {
+    //       console.log('Se completó la tarea');
+          
+    //     }
+    //   });
+    // }
+    
+    // console.log( event.target.files );
+
+    // // this.archivos.push(document);
+    // this.archivo = document;
+    // this.reportService.setArchivo( document );
   }
+
+  uploadFile() {
+    const data = JSON.stringify({
+      'token': this.tokenService.getToken(),
+      'data': {
+        'paciente': this.patientService.getIdPaciente(),
+        'DNI': this.patientService.getDniPaciente(),
+        'empresa': this.companyService.getIdEmpresa()
+      }
+    });
+
+    const formData2 = new FormData();
+
+    Array.prototype.forEach.call( this.files, (file: File) => {
+      formData2.append('document', file);
+    } );
+
+    formData2.append('json', data);
+
+    console.log(this.files);
+    console.log('-----------------------');
+    
+    console.log(data);
+    
+
+    this.reportService.uploadFile(formData2).subscribe({
+      next: (data) => {
+        console.log(data);
+        
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+        
+      },
+      complete: () => {
+        console.log('Se completó la tarea');
+        
+      }
+    });
+  }
+
+  // subirArchivo() {
+  //   // const formData = new FormData();
+  //   // formData.append('document', this.archivo);
+
+  //   // console.log( this.archivo );
+
+  //   this.reportService.uploadFile();
+
+  //   this.reportService.uploadFile().subscribe(
+  //     data => {
+  //       console.log( data );
+        
+  //     }
+  //   )
+  // }
 }
--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 import { PatientService } from './patient.service';
 import { CompanyService } from './company.service';
 import { TokenService } from './token.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+  public archivo: any;
 
   constructor(
     private httpClient: HttpClient,
@@ -16,18 +18,53 @@ export class ReportService {
     private tokenService: TokenService
   ) { }
 
-  public fileUpload() {
-    const data = JSON.stringify({
-      'token': this.tokenService.getToken(),
-      'data': {
-        'paciente': this.patientService.getIdPaciente(),
-        'DNI': this.patientService.getDniPaciente(),
-        'empresa': this.companyService.getIdEmpresa()
-      }
-    });
-
-    console.log( data );
+  public setArchivo( archivo: any ) {
+    this.archivo = archivo;
   }
+
+  public uploadFile( formData: FormData ): Observable<any> {
+    // const data = JSON.stringify({
+    //   'token': this.tokenService.getToken(),
+    //   'data': {
+    //     'paciente': this.patientService.getIdPaciente(),
+    //     'DNI': this.patientService.getDniPaciente(),
+    //     'empresa': this.companyService.getIdEmpresa()
+    //   }
+    // });
+
+    // const formData = new FormData();
+    // formData.append
+
+    // console.log( data );
+
+    return this.httpClient.post<FormData>('/fileUpload', formData);
+  }
+
+  // public uploadFile(): Observable<any> {
+  //   const data = JSON.stringify({
+  //     'token': this.tokenService.getToken(),
+  //     'data': {
+  //       'paciente': this.patientService.getIdPaciente(),
+  //       'DNI': this.patientService.getDniPaciente(),
+  //       'empresa': this.companyService.getIdEmpresa()
+  //     }
+  //   });
+
+  //   // console.log( this.archivo );
+    
+
+  //   const formData = new FormData();
+  //   formData.append('document', this.archivo);
+  //   // formData.append('json', data);
+    
+
+  //   // return this.httpClient.post('/fileUpload', formData, {
+  //   //   headers: {
+  //   //     'Content-Type': 'multipart/form-data'
+  //   //   }
+  //   // });
+  //   return this.httpClient.post<FormData>('/fileUpload', formData);
+  // }
 
   public getFile() {}
 }
