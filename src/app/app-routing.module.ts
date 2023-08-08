569c2c66c55b1366db8d8249aff7d8ee01a0f8a0
Braian Vargas <braianvargas1616@gmail.com>
2023-08-08 15:24:52 -0300

    [ADD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { IndexComponent } from './carga-informe/index/index.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ) },
   { path: 'home', loadChildren: () => import('./carga-informe/carga-informe.module').then( m => m.CargaInformeModule ) },
+  { path: 'usuarios', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule) },
+  { path: 'empresas', loadChildren: () => import('./empresas/empresas.module').then(m => m.EmpresasModule) },
   { path: '**', redirectTo: 'auth' }
 ];
 
